09ea2079e538adc992d837fc70d11a79b30292e1
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-10-10 21:55:46 -0300

    refactor: Restructure the project to apply Clean Architecture

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,20 +1,22 @@
 import { UniqueEntityId } from "~/core/entities/unique-entity-id";
 
-import { Answer } from "../entities/answer";
+import { Answer } from "../../enterprise/entities/answer";
 import { IAnswerRepository } from "../repositories/answer-repository";
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string;
   questionId: string;
-  content: string
+  content: string;
 }
 
 class AnswerQuestionUseCase {
-  constructor(
-    private answerRepository: IAnswerRepository
-  ) { }
-  
-  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+  constructor(private answerRepository: IAnswerRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityId(instructorId),
